725e0c34bb6b2f2177f9ce0860d157a940089998
Aditya Neve <aditya.neve@gmail.com>
2021-11-24 21:12:13 +0530

    routes added
    * see all movies
    * add a new movie
    * get a single movie
    * update a movie
    * delete a movie

--- movies-api/App.js
+++ movies-api/App.js
@@ -6,16 +6,13 @@ const app = express();
 app.use(express.json());
 
 /*
+    users
 
-users
-
-post = /users
-get all = /users
-get one = /users/:id
-update one = /users/:id
-delete one = /users/:id
-
-
+    post = /users
+    get all = /users
+    get one = /users/:id
+    update one = /users/:id
+    delete one = /users/:id
 */
 
 const connect = () => {
@@ -23,17 +20,26 @@ const connect = () => {
 };
 
 // first_name: { type: String, required: true },
-//   last_name: { type: String, required: false },
-//   email: { type: String, required: true, unique: true }
+// last_name: { type: String, required: false },
+// email: { type: String, required: true, unique: true }
 
 const userSchema = new mongoose.Schema({
-    movie_name: { type: String, unique: true, },
-    movie_genre: { type: String, required: true, },
+    movie_name: { type: String, unique: true },
+    movie_genre: { type: String, required: true },
     production_year: { type: Number, required: true, default: 2021 },
     budget: { type: Number, required: true },
 });
 
 const Movies = mongoose.model("movie", userSchema);
+
+app.get("/movies", async (req, res) => {
+    try {
+        const movie = await Movies.find().lean().exec();
+        return res.status(200).send(movie);
+    } catch (e) {
+        return res.status(500).send({ message: e.message, status: "Failed" });
+    }
+});
 
 app.post("/movies", async (req, res) => {
     try {
@@ -81,44 +87,3 @@ app.listen(3001, async () => {
     await connect();
     console.log("Listening on port 3001");
 });
-
-/*
-const express = require("express");
-const mongoose = require("mongoose");
-
-const connect = () => {
-    return mongoose.connect("mongodb://127.0.0.1:27017/movies");
-};
-
-const movieSchema = new mongoose.Schema(
-    {
-        movie_name: { type: String, required: true },
-        movie_genre: { type: String, required: true, unique: true },
-        production_year: { type: Number, required: false, default: 2021 },
-        budget: { type: Number, required: true },
-    },
-    {
-        versionKey: false,
-        timestamps: true,
-    }
-);
-
-const app = express();
-app.use(express.json());
-
-const Movies = mongoose.model("movie", movieSchema);
-
-app.post("/movies", async (req, res) => {
-    try {
-        const movie = await Movies.create(req.body);
-        return res.status(201).send(movie);
-    } catch (e) {
-        return res.status(500).send({ message: e.message });
-    }
-});
-
-app.listen(3001, async () => {
-    await connect();
-    console.log("Listening on port 3001");
-});
-*/
