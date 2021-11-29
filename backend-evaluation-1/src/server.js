aa008f19d3ded132dda4a569a1113bbef1491b8c
Aditya Neve <aditya.neve@gmail.com>
2021-11-29 15:49:41 +0530

    backend eval 1

---
+++ backend-evaluation-1/src/server.js
@@ -0,0 +1,123 @@
+const mongoose = require("mongoose");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const connect = () => {
+    return mongoose.connect("mongodb://127.0.0.1:27017/naukri");
+};
+
+const jobsSchema = new mongoose.Schema(
+    {
+        job_name: { type: String, required: true },
+        work_from_home: { type: Boolean, required: false, default: true },
+        notice_period: { type: String, required: true, default: "2m" },
+        rating: { type: Number },
+        city_name: { type: String, required: true },
+        company_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "company",
+            required: true,
+        },
+    },
+    {
+        versionKey: false,
+        timestamps: true,
+    }
+);
+const Job = new mongoose.model("job", jobsSchema);
+
+const companySchema = new mongoose.Schema(
+    {
+        company_name: { type: String, required: true },
+        company_address: { type: String, required: true },
+        company_contact: { type: Number, required: false },
+    },
+    {
+        versionKey: false,
+        timestamps: true,
+    }
+);
+const Company = new mongoose.model("company", companySchema);
+
+const skillSchema = new mongoose.Schema(
+    {
+        skill: { type: String },
+        job_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "job",
+            required: true,
+        },
+    },
+    {
+        versionKey: false,
+        timestamps: true,
+    }
+);
+const Skill = new mongoose.model("skill", skillSchema);
+
+app.post("/company", async (req, res) => {
+    try {
+        const company = await Company.create(req.body);
+        return res.status(201).send(company);
+    } catch (e) {
+        return res.status(500).send({ message: e.message });
+    }
+});
+
+app.post("/job", async (req, res) => {
+    try {
+        const job = await Job.create(req.body);
+        return res.status(201).send(job);
+    } catch (e) {
+        return res.status(500).send({ message: e.message });
+    }
+});
+
+app.post("/skill", async (req, res) => {
+    try {
+        const job = await Skill.create(req.body);
+        return res.status(201).send(job);
+    } catch (e) {
+        return res.status(500).send({ message: e.message });
+    }
+});
+
+app.get("/jobs/skill=:skill", async (req, res) => {
+
+    try {
+        const jobs = await Skill.find({ skill: req.params.skill})
+        return res.status(201).send(jobs);
+    } catch (e) {
+        return res.status(500).send({ message: e.message });
+    }
+
+});
+
+app.get("/jobs/work-form-home", async (req, res) => {
+
+    try {
+        const jobs = await Job.find({ work_from_home: true})
+        return res.status(201).send(jobs);
+    } catch (e) {
+        return res.status(500).send({ message: e.message });
+    }
+
+});
+
+app.get("/jobs/notice-period", async (req, res) => {
+
+    try {
+        const jobs = await Job.find({ notice_period: "2m"})
+        return res.status(201).send(jobs);
+    } catch (e) {
+        return res.status(500).send({ message: e.message });
+    }
+
+});
+
+app.listen(3001, async () => {
+    await connect();
+    console.log("LISTENING ON SERVER 3001.");
+});
