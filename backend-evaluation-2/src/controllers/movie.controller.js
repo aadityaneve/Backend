480573466a90a0f5a56eace86a60dbee5bd92ffa
Aditya Neve <aditya.neve@gmail.com>
2021-12-13 15:18:56 +0530

    models and controllers created

---
+++ backend-evaluation-2/src/controllers/movie.controller.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const Movie = require('../models/movie.model');
+
+router.post('/movie', async (req, res) => {
+    try {
+        const movie = await Movie.create(req.body);
+
+        return res.status(200).json({ status: 'Success', movie: movie });
+    } catch (e) {
+        return res.status(500).json({ status: 'Failed', message: e.message });
+    }
+});
+
+router.get('/movies', async (req, res) => {
+    try {
+        const movie = await Movie.find(req.body).lean().exec();
+
+        return res.status(200).json({ status: 'Success', movie: movie });
+    } catch (e) {
+        return res.status(500).json({ status: 'Failed', message: e.message });
+    }
+});
+
+module.exports = router;
